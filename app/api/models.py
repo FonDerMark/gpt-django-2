8ee019212d3bbe4210ad17f1a8c69cbe5a3b1b5e
Xm0rph <publicnox@gmail.com>
2023-05-13 21:00:24 +0500

    0.0.9

--- app/api/models.py
+++ app/api/models.py
@@ -9,6 +9,7 @@ class TelegramUser(models.Model):
     day_limit_of_messages = models.IntegerField(null=True)
     day_limit = models.DateField(null=True)
     premium_status = models.DateTimeField(null=True, blank=True)
+    banned = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return f'{self.username}({self.user_id})/limit:{self.day_limit_of_messages}'
